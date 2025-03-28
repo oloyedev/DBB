1fc736816986d9489b072b0d1cf76e386dc31f0a
oloyedev <104859747+oloyedev@users.noreply.github.com>
2025-03-28 08:24:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
+CORS(app)
 # PostgreSQL Connection
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
